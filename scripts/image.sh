148357b3c9b50c73e2a7eaef4f7a287480c42f45
Michael Clark <mjc@sifive.com>
2017-12-26 14:34:48 +1300

    Create additional system directories and minimal /dev entries.
    Author: Karsten Merker <merker@debian.org>

--- scripts/image.sh
+++ scripts/image.sh
@@ -12,7 +12,7 @@ dd if=/dev/zero of=${IMAGE_FILE} bs=1M count=${IMAGE_SIZE}
 /sbin/mkfs.ext4 -j -F ${IMAGE_FILE}
 test -d mnt || mkdir mnt
 mount -o loop ${IMAGE_FILE} mnt
-( cd mnt && mkdir -p root bin dev lib proc sbin sys tmp var/run var/log var/tmp etc/dropbear etc/network/if-pre-up.d etc/network/if-up.d etc/network/if-down.d etc/network/if-post-down.d )
+( cd mnt && mkdir -p root bin dev lib lib/modules proc sbin sys tmp usr usr/bin usr/sbin var/run var/log var/tmp etc/dropbear etc/network/if-pre-up.d etc/network/if-up.d etc/network/if-down.d etc/network/if-post-down.d )
 cp build/busybox-${BUSYBOX_VERSION}/busybox mnt/bin/
 cp build/dropbear-${DROPBEAR_VERSION}/dropbear mnt/sbin/
 rsync -a --exclude ldscripts --exclude '*.la' --exclude '*.a' ${RISCV}/sysroot/lib/ mnt/lib/
@@ -25,5 +25,8 @@ touch mnt/var/log/wtmp
 ln -s ../bin/busybox mnt/sbin/init
 ln -s busybox mnt/bin/sh
 cp bin/ldd mnt/bin/ldd
+mknod mnt/dev/console c 5 1
+mknod mnt/dev/ttyS0 c 4 64
+mknod mnt/dev/null c 1 3
 umount mnt
 rmdir mnt
