5427ac14e3f85ceafd38e3c15d92c3f62429e7f7
Michael Clark <mjc@sifive.com>
2018-08-20 21:13:28 +1200

    Make changes to work with multilib toolchain

--- scripts/image.sh
+++ scripts/image.sh
@@ -12,10 +12,11 @@ dd if=/dev/zero of=${IMAGE_FILE} bs=1M count=${IMAGE_SIZE}
 /sbin/mkfs.ext4 -j -F ${IMAGE_FILE}
 test -d mnt || mkdir mnt
 mount -o loop ${IMAGE_FILE} mnt
-( cd mnt && mkdir -p root bin dev lib lib/modules proc sbin sys tmp usr usr/bin usr/sbin var/run var/log var/tmp etc/dropbear etc/network/if-pre-up.d etc/network/if-up.d etc/network/if-down.d etc/network/if-post-down.d )
+( cd mnt && mkdir -p root bin dev lib lib64 lib/modules proc sbin sys tmp usr usr/bin usr/sbin var/run var/log var/tmp etc/dropbear etc/network/if-pre-up.d etc/network/if-up.d etc/network/if-down.d etc/network/if-post-down.d )
 cp build/busybox-${BUSYBOX_VERSION}/busybox mnt/bin/
 cp build/dropbear-${DROPBEAR_VERSION}/dropbear mnt/sbin/
 rsync -a --exclude ldscripts --exclude '*.la' --exclude '*.a' ${RISCV}/sysroot/lib/ mnt/lib/
+rsync -a --exclude ldscripts --exclude '*.la' --exclude '*.a' ${RISCV}/sysroot/lib64/ mnt/lib64/
 rsync -a etc/ mnt/etc/
 hash=$(openssl passwd -1 -salt xyzzy ${ROOT_PASSWORD})
 sed -i'' "s:\*:${hash}:" mnt/etc/shadow
