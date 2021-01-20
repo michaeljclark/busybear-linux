76f026b08b3f0b3c1088a3b9f1641a937900ddf0
Saverio Miroddi <saverio.pub2@gmail.com>
2021-01-20 23:00:24 +0100

    ifup.sh: Add helpful message for when the `brctl` command is not found
    In Ubuntu [20.04] for example, the package is not installed by default.

--- scripts/ifup.sh
+++ scripts/ifup.sh
@@ -2,5 +2,10 @@
 
 set -o errexit
 
+if ! [ -x "$(command -v brctl)" ]; then
+  >&2 echo '`brctl` could not be found; please double check that the package is installed.'
+  exit 1
+fi
+
 brctl addif virbr0 $1
 ifconfig $1 up
